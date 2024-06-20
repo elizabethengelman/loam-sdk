fd2647e8e02e5bf859e8d363795739eff51c3b7a
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-06-20 15:18:51 -0400

    Testing npm workflow

--- npm/src/getBinary.ts
+++ npm/src/getBinary.ts
@@ -32,7 +32,7 @@ function getPlatform() {
 
 export function GithubUrl(): string {
   const [platform, arch] = getPlatform();
-  return `https://github.com/loambuild/loam-sdk/releases/download/v${version}/${NAME}-v${version}-${arch}-${platform}.tar.gz`;
+  return `https://github.com/elizabethengelman/loam-sdk/releases/download/v${version}/${NAME}-v${version}-${arch}-${platform}.tar.gz`;
 }
 
 export function getBinary(name: string = NAME): Promise<Binary> {
